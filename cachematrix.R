a8e6f857f3f5a24849998a3a4381587622fcc3ca
jluciove <jluciove@udel.edu>
2020-08-12 13:45:14 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a vector of functions
+## and caches x matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes vector x and uses its functions and
+## cache to find the inverse of A
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  # Solve for Inverse
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
+
+## Calling functions section
+
+#  Matrix initializing, square matrix
+A = matrix(
+  c(2, 4, 3, 1, 5, 7, 1, 2, 4), # the data elements 
+  nrow=3,              # number of rows 
+  ncol=3,              # number of columns 
+  byrow = TRUE)        # fill matrix by rows
+
+det(A) # Must have a determinant
+
+x <- makeCacheMatrix(A)
+i <- cacheSolve(x)
